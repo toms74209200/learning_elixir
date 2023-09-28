8c605609a4b6226685602da95f1765cffe19e587
toms74209200 <motomotomato@gmail.com>
2023-09-29 00:34:09 +0900

    :fire: delete unnecessary module

--- sequence3/lib/sequence3/impl.ex
+++
@@ -1,7 +0,0 @@
-defmodule Sequence.Impl do
-  def next(number), do: number + 1
-  def increment(number, delta), do: number + delta
-  def push(stack, item), do: [item | stack]
-  def pop([]), do: {:empty, []}
-  def pop([head | tail]), do: {head, tail}
-end
